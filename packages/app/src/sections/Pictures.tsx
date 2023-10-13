a3fbbb11c0e24fade27ec259dde82a73fe90ccd5
Kathan Cheema <cheemcheem@users.noreply.github.com>
2023-10-13 23:32:57 +0000

    Update pictures text

--- packages/app/src/sections/Pictures.tsx
+++ packages/app/src/sections/Pictures.tsx
@@ -7,9 +7,10 @@ const picturesUrl =
 
 export const Pictures: React.FC = () => (
   <StackCard style={{ height: '100%' }}>
-    <Text style={{ textAlign: 'center' }}>
+    <Text>
       Photos of our wedding day can be viewed at the following link:{' '}
-      <Link href={picturesUrl}>wedding photos</Link>.
+      <Link href={picturesUrl}>wedding photos</Link>. All of the photographs
+      displayed at this link can be downloaded by any user free of charge.
     </Text>
   </StackCard>
 );
